ccf12c28ec9ab42ef0a7aa278d22141565cc0065
samiltv09 <148400103+samiltv09@users.noreply.github.com>
2023-12-06 13:55:02 -0400

    Add files via upload

---
+++ js/javas.js
@@ -0,0 +1,15 @@
+let day = document.getElementById("day");
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+setInterval(()=>{
+    let currenTime = new Date();
+
+    day.innerHTML = (currenTime.getDay()) + currenTime.getDay();
+    hrs.innerHTML = (currenTime.getHours()<10?"0":"") + currenTime.getHours();
+    min.innerHTML = (currenTime.getMinutes()<10?"0":"") + currenTime.getMinutes();
+    sec.innerHTML = (currenTime.getSeconds()<10?"0":"") + currenTime.getSeconds();
+
+},1000)
+
